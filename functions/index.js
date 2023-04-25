321495c29ca15cf44add7308268278e69707c0f1
Rishik <56663764+Rishik-Yechuri@users.noreply.github.com>
2023-04-25 11:49:52 -0500

    Created register function
    -Registers user and sends back temporary token

---
+++ functions/index.js
@@ -0,0 +1,25 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+
+exports.registerUser = functions.https.onRequest(async (req, res) => {
+    if (req.method !== 'POST') {
+        res.status(405).send('Method Not Allowed');
+        return;
+    }
+
+    const { email, password } = req.body;
+
+    try {
+        const userRecord = await admin.auth().createUser({ email, password });
+        const customToken = await admin.auth().createCustomToken(userRecord.uid);
+
+        // Exchange the custom token for an ID token
+        const decodedToken = await admin.auth().verifyIdToken(customToken);
+        const idToken = decodedToken.token;
+        res.status(200).send({ success: true, token: idToken });
+    } catch (error) {
+        res.status(400).send({ success: false, error: error.message });
+    }
+});
