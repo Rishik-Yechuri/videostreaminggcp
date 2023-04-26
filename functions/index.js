ba3bfbe26ecaa521ee57bfee0699ed7dfebe9fee
Rishik <56663764+Rishik-Yechuri@users.noreply.github.com>
2023-04-26 13:56:28 -0500

    Updated sign in function
    -Fixed bug that prevented sign in function from returning token

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const axios = require('axios');
 admin.initializeApp();
 
 
@@ -33,14 +34,17 @@ exports.signInUser = functions.https.onRequest(async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        const userRecord = await admin.auth().getUserByEmail(email);
-        await admin.auth().updateUser(userRecord.uid, { password });
+        const apiKey = 'AIzaSyDf-zcmQxzLO1vq1zyWfokwqEJ3c_gNubI'; // Replace with your Firebase API Key
 
-        const customToken = await admin.auth().createCustomToken(userRecord.uid);
+        // Authenticate user and retrieve ID token using Google Identity Platform REST API
+        const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`, {
+            email,
+            password,
+            returnSecureToken: true,
+        });
 
-        // Exchange the custom token for an ID token
-        const decodedToken = await admin.auth().verifyIdToken(customToken);
-        const idToken = decodedToken.token;
+        const idToken = response.data.idToken;
+
         res.status(200).send({ success: true, token: idToken });
     } catch (error) {
         res.status(400).send({ success: false, error: error.message });
