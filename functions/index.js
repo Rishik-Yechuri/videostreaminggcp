1041cc615aab59e69d2a6cf7a82c01c8809a1b15
Rishik <56663764+Rishik-Yechuri@users.noreply.github.com>
2023-04-26 14:02:15 -0500

    Fixed bug in registerUser

--- functions/index.js
+++ functions/index.js
@@ -14,11 +14,21 @@ exports.registerUser = functions.https.onRequest(async (req, res) => {
 
     try {
         const userRecord = await admin.auth().createUser({ email, password });
-        const customToken = await admin.auth().createCustomToken(userRecord.uid);
+        //const customToken = await admin.auth().createCustomToken(userRecord.uid);
 
         // Exchange the custom token for an ID token
-        const decodedToken = await admin.auth().verifyIdToken(customToken);
-        const idToken = decodedToken.token;
+       // const decodedToken = await admin.auth().verifyIdToken(customToken);
+        //const idToken = decodedToken.token;
+        const apiKey = 'AIzaSyDf-zcmQxzLO1vq1zyWfokwqEJ3c_gNubI'; // Replace with your Firebase API Key
+
+        // Authenticate user and retrieve ID token using Google Identity Platform REST API
+        const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`, {
+            email,
+            password,
+            returnSecureToken: true,
+        });
+
+        const idToken = response.data.idToken;
         res.status(200).send({ success: true, token: idToken });
     } catch (error) {
         res.status(400).send({ success: false, error: error.message });
