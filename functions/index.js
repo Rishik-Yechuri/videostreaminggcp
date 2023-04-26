fe9da81638aa2de0a5c003f5f59fc243ff2e7a60
Rishik <56663764+Rishik-Yechuri@users.noreply.github.com>
2023-04-25 23:50:05 -0500

    Added sign in

--- functions/index.js
+++ functions/index.js
@@ -23,3 +23,26 @@ exports.registerUser = functions.https.onRequest(async (req, res) => {
         res.status(400).send({ success: false, error: error.message });
     }
 });
+
+exports.signInUser = functions.https.onRequest(async (req, res) => {
+    if (req.method !== 'POST') {
+        res.status(405).send('Method Not Allowed');
+        return;
+    }
+
+    const { email, password } = req.body;
+
+    try {
+        const userRecord = await admin.auth().getUserByEmail(email);
+        await admin.auth().updateUser(userRecord.uid, { password });
+
+        const customToken = await admin.auth().createCustomToken(userRecord.uid);
+
+        // Exchange the custom token for an ID token
+        const decodedToken = await admin.auth().verifyIdToken(customToken);
+        const idToken = decodedToken.token;
+        res.status(200).send({ success: true, token: idToken });
+    } catch (error) {
+        res.status(400).send({ success: false, error: error.message });
+    }
+});
