4bdd66a8f34c065f74c496ec418cb2f49c255ace
Rishik <56663764+Rishik-Yechuri@users.noreply.github.com>
2023-05-03 17:09:37 -0500

    Video Uploading Works
    -Function created to set metadata
    -Returns signed urls for client to upload video and thumbnail

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,28 @@
 const functions = require('firebase-functions');
-const cors = require('cors')({ origin: true });
-const { v4: uuidv4 } = require('uuid');
-const multer = require('multer');
-
 const admin = require('firebase-admin');
+
 const axios = require('axios');
+
+const { v4: uuidv4 } = require('uuid');
+const { IncomingForm } = require('formidable');
+const Multer = require('multer');
 const { Storage } = require('@google-cloud/storage');
-const { Firestore } = require('@google-cloud/firestore');
-const fs = require("fs");
-admin.initializeApp();
-const firestore = new Firestore();
-/*const storage = new Storage();
+const storage = new Storage();
 const bucket = storage.bucket('holdvideos');
-const storage = multer.memoryStorage();
-const upload = multer({ storage });*/
-const bucketName = 'holdvideos'; // Replace with your actual bucket name
-const bucket = admin.storage().bucket(bucketName);
-const upload = multer();
 
+admin.initializeApp();
+const firestore = admin.firestore();
 
-const storage = multer.memoryStorage();
+const upload = Multer({
+    storage: Multer.memoryStorage(),
+    limits: {
+        fileSize: 50 * 1024 * 1024,
+    },
+});
+const runtimeOpts = {
+    timeoutSeconds: 540, // Increase the timeout to 60 seconds
+};
+
 exports.registerUser = functions.https.onRequest(async (req, res) => {
     if (req.method !== 'POST') {
         res.status(405).send('Method Not Allowed');
@@ -51,7 +54,7 @@ exports.registerUser = functions.https.onRequest(async (req, res) => {
     }
 });
 
-exports.signInUser = functions.https.onRequest(async (req, res) => {
+exports.signInUser = functions.runWith(runtimeOpts).https.onRequest(async (req, res) => {
     if (req.method !== 'POST') {
         res.status(405).send('Method Not Allowed');
         return;
@@ -76,98 +79,13 @@ exports.signInUser = functions.https.onRequest(async (req, res) => {
         res.status(400).send({ success: false, error: error.message });
     }
 });
-/*exports.uploadVideo = functions.https.onRequest(async (req, res) => {
-    cors(req, res, async () => {
-        if (req.method !== 'POST') {
-            res.status(405).send('Method Not Allowed');
-            return;
-        }
-
-        const { title, description, userId } = req.body;
-
-        upload.single('video')(req, res, async (err) => {
-            if (err) {
-                res.status(400).send({ success: false, error: err.message });
-                return;
-            }
-
-            const videoFile = req.file;
-
-            if (!videoFile) {
-                res.status(400).send({ success: false, error: 'No video file provided' });
-                return;
-            }
-
-            try {
-                const videoId = uuidv4();
-                const fileName = `${userId}/${videoId}.mp4`; // Assuming .mp4 format
-                const file = bucket.file(fileName);
-
-                const writeStream = file.createWriteStream({ resumable: false });
-
-                writeStream.on('error', (error) => {
-                    res.status(500).send({ success: false, error: error.message });
-                });
-
-                writeStream.on('finish', async () => {
-                    // Store video metadata in Firestore
-                    const videoRef = firestore.collection('videos').doc(videoId);
-
-                    const videoMetadata = {
-                        title,
-                        description,
-                        userId,
-                        privacy: 'public', // Default privacy setting
-                        createdAt: admin.firestore.FieldValue.serverTimestamp(),
-                        views: 0,
-                        likes: 0,
-                        dislikes: 0,
-                    };
-
-                    await videoRef.set(videoMetadata);
-
-                    res.status(200).send({ success: true, videoId });
-                });
-
-                writeStream.end(videoFile.buffer);
-            } catch (error) {
-                res.status(400).send({ success: false, error: error.message });
-            }
-        });
-    });
-});*/
-exports.uploadVideo = functions.https.onRequest(async (req, res) => {
+exports.generateSignedUrl = functions.https.onRequest(async (req, res) => {
     if (req.method !== 'POST') {
         res.status(405).send('Method Not Allowed');
         return;
     }
 
-    const { email, password } = req.body;
-
-    try {
-        const apiKey = 'AIzaSyDf-zcmQxzLO1vq1zyWfokwqEJ3c_gNubI'; // Replace with your Firebase API Key
-
-        // Authenticate user and retrieve ID token using Google Identity Platform REST API
-        const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`, {
-            email,
-            password,
-            returnSecureToken: true,
-        });
-
-        const idToken = response.data.idToken;
-
-        res.status(200).send({ success: true, token: idToken });
-    } catch (error) {
-        res.status(400).send({ success: false, error: error.message });
-    }
-});
-exports.videoUpload = functions.https.onRequest(async (req, res) => {
-    if (req.method !== 'POST') {
-        res.status(405).send('Method Not Allowed');
-        return;
-    }
-
-    const authToken = req.headers.authorization && req.headers.authorization.split('Bearer ')[1];
+    const authToken = req.headers.authorization;
 
     if (!authToken) {
         res.status(401).send('Unauthorized: No token provided');
@@ -175,44 +93,56 @@ exports.videoUpload = functions.https.onRequest(async (req, res) => {
     }
 
     try {
+        console.log("Received token:", authToken);
         const decodedToken = await admin.auth().verifyIdToken(authToken);
+        console.log("Decoded token:", decodedToken);
         const userId = decodedToken.uid;
-        upload.single('video')(req, res, async (err) => {
-            if (err) {
-                res.status(400).send({ success: false, error: err.message });
-                return;
-            }
+        const metadata = req.body.metadata;
 
-            const videoFile = req.file;
+        const signedUrlOptionsVideo = {
+            version: 'v4',
+            action: 'write',
+            expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+            contentType: 'video/mp4',
+        };
 
-            if (!videoFile) {
-                res.status(400).send({ success: false, error: 'No video file provided' });
-                return;
-            }
+        const signedUrlOptionsThumbnail = {
+            version: 'v4',
+            action: 'write',
+            expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+            contentType: 'image/jpeg',
+        };
 
-            try {
-               // const userId = 'some_user_id'; // Replace with the actual user ID
-                const videoId = uuidv4();
-                const fileName = `${userId}/${videoId}.mp4`; // Assuming .mp4 format
-                const file = bucket.file(fileName);
 
-                const writeStream = file.createWriteStream({ resumable: false });
+        const videoId = uuidv4();
+        const videoFileName = `${userId}/${videoId}.mp4`; // Assuming .mp4 format
+        const videoFile = bucket.file(videoFileName);
+        const videoSignedUrl = await videoFile.getSignedUrl(signedUrlOptionsVideo);
 
-                writeStream.on('error', (error) => {
-                    res.status(500).send({ success: false, error: error.message });
-                });
+        const thumbnailFileName = `${userId}/${videoId}.jpg`; // Assuming .jpg format
+        const thumbnailFile = bucket.file(thumbnailFileName);
+        const thumbnailSignedUrl = await thumbnailFile.getSignedUrl(signedUrlOptionsThumbnail);
 
-                writeStream.on('finish', () => {
-                    res.status(200).send({ success: true, videoId });
-                });
+        // Save metadata in Firestore
+        await admin.firestore().collection('videos').doc(videoId).set({
+            title: metadata.title,
+            description: metadata.description,
+            likes: 0,
+            dislikes: 0,
+            views: 0,
+            channelName: userId,
+            userId: userId,
+            videoUrl: videoFileName,
+            thumbnailUrl: thumbnailFileName,
+        });
 
-                writeStream.end(videoFile.buffer);
-            } catch (error) {
-                res.status(400).send({ success: false, error: error.message });
-            }
+        res.status(200).send({
+            success: true,
+            videoId,
+            videoSignedUrl: videoSignedUrl[0],
+            thumbnailSignedUrl: thumbnailSignedUrl[0],
         });
     } catch (error) {
-        res.status(401).send('Unauthorized: Invalid token');
-        return;
+        res.status(401).send('Unauthorized: Invalid token,' + authToken + " Error: " + error);
     }
 });
